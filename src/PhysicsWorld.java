6793ccb1a84f851f80929c2a05249cca0ebb0275
Brian Edmonds <brianedmonds90@gmail.com>
2013-03-25 15:47:07 -0400

    Physics World

--- src/PhysicsWorld.java
+++ src/PhysicsWorld.java
@@ -1,4 +1,11 @@
+import org.jbox2d.collision.AABB;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.World;
+
 
 public class PhysicsWorld {
-
+	private World world;
+	public PhysicsWorld(){
+		world=new World(new AABB(),new Vec2(0,0), false);
+	}
 }
